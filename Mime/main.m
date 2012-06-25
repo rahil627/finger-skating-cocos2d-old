30fa4a8d886a7ad4fd8e9e62acd00bb8df34ab14
Rahil Patel <rahil627@gmail.com>
2012-06-25 16:03:54 -0400

    Initial Commit

---
+++ Mime/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Mime
+//
+//  Created by Rahil Patel on 6/25/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
