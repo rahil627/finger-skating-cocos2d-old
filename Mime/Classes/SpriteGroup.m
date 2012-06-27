cc7630354c301c87a50152bf9c91e913b3d31bac
Rahil Patel <rahil627@gmail.com>
2012-06-27 01:44:03 -0400

    huh, maybe it will work!

--- Mime/Classes/SpriteGroup.m
+++ Mime/Classes/SpriteGroup.m
@@ -22,4 +22,18 @@
     return self;
 }
 
+- (void)draw {
+    // draw a line between each sprite
+    if ([[self children] count] < 2)
+        return;
+    
+    for (int i = 0; i < [[self children] count] - 1; i++) {
+        CGPoint p1 = ((CCNode*)[[self children] objectAtIndex:i]).position;
+        CGPoint p2 = ((CCNode*)[[self children] objectAtIndex:i + 1]).position;
+        ccDrawLine(p1, p2);
+    }
+    
+    [super draw];
+}
+
 @end
